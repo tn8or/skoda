6c7d878f46eeb24441e0b301a579dc92d5109c7d
Tommy Eriksen <tommy.eriksen@gmail.com>
2025-07-09 19:13:43 +0200

    updated db schema

--- sqldump/sqldump.sql
+++ sqldump/sqldump.sql
@@ -31,7 +31,11 @@ CREATE TABLE `charge_events` (
   `mileage` int(11) DEFAULT NULL,
   `pos_lat` varchar(10) DEFAULT NULL,
   `pos_lon` varchar(10) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `soc` int(3) DEFAULT NULL,
+  `charge_id` varchar(36) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `time` (`event_timestamp`),
+  KEY `charge_id` (`charge_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
@@ -61,12 +65,39 @@ DROP TABLE IF EXISTS `charge_hours`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8mb4 */;
 CREATE TABLE `charge_hours` (
+  `id` varchar(36) NOT NULL,
   `log_timestamp` timestamp NOT NULL,
-  `amount` int(11) NOT NULL,
-  `position` varchar(45) NOT NULL,
-  PRIMARY KEY (`log_timestamp`)
+  `amount` float DEFAULT NULL,
+  `position` varchar(45) DEFAULT NULL,
+  `soc` tinyint(3) DEFAULT NULL,
+  `price` float DEFAULT NULL,
+  `endrecord` enum('(''start'',''stop'')') DEFAULT NULL COMMENT '		',
+  `charged_range` int(10) DEFAULT NULL,
+  `mileage` int(10) DEFAULT NULL,
+  `start_at` datetime DEFAULT NULL,
+  `stop_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `time` (`log_timestamp`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_uca1400_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`skoda`@`%`*/ /*!50003 TRIGGER before_insert_event_uuid
+BEFORE INSERT ON skoda.charge_hours
+FOR EACH ROW
+SET NEW.id = UUID() */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
 -- Table structure for table `rawlogs`
@@ -77,7 +108,9 @@ DROP TABLE IF EXISTS `rawlogs`;
 /*!40101 SET character_set_client = utf8mb4 */;
 CREATE TABLE `rawlogs` (
   `log_timestamp` timestamp NULL DEFAULT NULL,
-  `log_message` text DEFAULT NULL
+  `log_message` text DEFAULT NULL,
+  KEY `message` (`log_message`(768)),
+  KEY `time` (`log_timestamp`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -90,4 +123,4 @@ CREATE TABLE `rawlogs` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*M!100616 SET NOTE_VERBOSITY=@OLD_NOTE_VERBOSITY */;
 
--- Dump completed on 2025-07-08  8:33:10
+-- Dump completed on 2025-07-09 19:13:17
